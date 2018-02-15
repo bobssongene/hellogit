7227fee5316f76981597141a561dc84180fb4780
bobssongene <marcxavier84@gmail.com>
2018-02-15 13:56:23 -0400

    testedval.h

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "testedval.h"
+
+using namespace std;
+
+int main()
+{
+TestedVals obj;
+
+obj.set(8)
+obj.set(16.54)
+
+obj.addition(50,65);// checking the function to see if it works 
+obj.subtraction(8465,6560)
+cout << endl;
+obj.addition(obj.getVal, obj.getDval);
+obj.subtraction(obj.getVal, get.getDval);
+return 0;
+}
---
+++ testedval.h
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+
+class TestedVals
+{
+int val;
+double dval;
+bool test;
+
+public:
+TestedVals()
+{
+val = dval= 0;
+test = false;
+}
+
+void set(int x)
+{
+val = x;
+}
+
+void set(double x)
+{
+dval = x;
+}
+int getVal()
+{
+return val;
+}
+double getDval()
+{
+return dval;}
+
+double addition(auto a,auto b)
+{
+return (a+b);
+}
+double subtraction(auto a, auto b)
+{
+return (a-b)}
+
+
+void mkvect()
+{
+vector <int> array;
+}
+void addVectVals()
+{
+mkvect();
+test = true;
+auto x;
+if (test){
+while (test){
+cout<< "enter n if you want to end this loop" <<endl;
+cout << "val:" << endl;
+cin >> x;
+if(x != 'n'){
+array.push_back(x);
+}
+else 
+test = false;
+}
+}
+}
+
+
+void dispVect()
+{
+for(int i, array)
+{
+cout << array[i] << endl;
+}
+}
+
+}
