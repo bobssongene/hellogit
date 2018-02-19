291eb51b7d0f033d4fbacb61a95d23f9ca69da4e
bobssongene <marcxavier84@gmail.com>
2018-02-19 19:23:15 -0400

    testedval.h

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ vector <int> vectArray;
 
 obj.set(8);
 obj.set(16.54);
-cout << "test"<<endl;
+
 obj.addition(50,65);// checking the function to see if it works 
 obj.subtraction(8465,6560);
 cout << endl;
@@ -17,7 +17,7 @@ cout << obj.getVal() << " " << obj.getDval()<< endl;
 obj.addition(obj.getVal(), obj.getDval());
 obj.subtraction(obj.getVal(), obj.getDval());
 
-obj.testx();
+
 
 
 
